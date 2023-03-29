e74d32cd1d86f804486584d48840d4ef51adbf73
development <dev@ferrumgate.com>
2023-03-29 16:36:17 +0300

    added new ip intelligence implementation

--- docker.build.sh
+++ docker.build.sh
@@ -6,7 +6,7 @@ rm -rf node_modules/rest.portal2
 mkdir -p node_modules/rest.portal2
 cp -R node_modules/rest.portal/* node_modules/rest.portal2
 version=$(cat package.json | grep version | cut -d: -f2 | tr -d , | tr -d \" | tr -d " ")
-docker build -t job.log .
+docker build -t job.log . $1
 docker tag job.log job.log:$version
 echo "job.log:$version builded"
 docker tag job.log registry.ferrumgate.zero/ferrumgate/job.log:$version
